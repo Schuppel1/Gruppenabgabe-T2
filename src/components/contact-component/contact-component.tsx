f96a4be0b3509c0d93191416cfe0235551a962c6
Schuppel1 <sven.schu.mobil@gmail.com>
2022-05-18 08:37:18 +0200

    style fix

--- src/components/contact-component/contact-component.tsx
+++ src/components/contact-component/contact-component.tsx
@@ -15,42 +15,42 @@ export class ContactComponent {
   @Prop() dsgvotext: string;
   @Prop() mailtarget: string;
   @Prop() mailfrom: string;
-  @Prop() secondheader:string;
+  @Prop() secondheader: string;
 
   render() {
     return (
       <Host>
 
-<div>
+        <div>
           <h1 class="contact">{this.secondheader}</h1>
-          </div>
+        </div>
 
         <div class="contact-container">
-        <form id="contact-form" method="POST" accept-charset="UTF-8">
-          <fieldset>
-            <label>
-              {this.namelabel}
-              <input type="text" name="Name" required></input>
-            </label>
-            <label>
-              {this.emaillabel}
-              <input type="email" name="E-Mail" required></input>
-            </label>
-            <label>
-              {this.msglabel}
-              <textarea name="Nachricht" required></textarea>
-            </label>
-            <div>
+          <form id="contact-form" method="POST" accept-charset="UTF-8">
+            <fieldset>
               <label>
-                <input type="checkbox" name="Datenverarbeitung bestätigt" value="Ja" required></input>
-                {this.dsgvotext}
+                {this.namelabel}
+                <input type="text" name="Name" required></input>
               </label>
+              <label>
+                {this.emaillabel}
+                <input type="email" name="E-Mail" required></input>
+              </label>
+              <label>
+                {this.msglabel}
+                <textarea name="Nachricht" required></textarea>
+              </label>
+              <div>
+                <label>
+                  <input type="checkbox" name="Datenverarbeitung bestätigt" value="Ja" required></input>
+                  {this.dsgvotext}
+                </label>
+              </div>
+            </fieldset>
+            <div class="btns">
+              <input type="submit" value="Absenden"></input>
             </div>
-          </fieldset>
-          <div class="btns">
-            <input type="submit" value="Absenden"></input>
-          </div>
-        </form>
+          </form>
         </div>
         <slot></slot>
       </Host>
@@ -61,14 +61,14 @@ export class ContactComponent {
   }
 }
 
-function submitData(){
+function submitData() {
   alert("Die Email wurde versendet!. Vielen Dank")
 }
 
 
 function initializEventlistener() {
-  const componentElement =  document.querySelector("contact-component").shadowRoot;
-  componentElement.querySelector("#contact-form").addEventListener("submit",submitData);
+  const componentElement = document.querySelector("contact-component").shadowRoot;
+  componentElement.querySelector("#contact-form").addEventListener("submit", submitData);
 
 }
 
